6892dcee77c8ebce6d0c32af6c0e203b1bfaef26
Vishal <vishalshaw1998@gmail.com>
2020-09-07 02:41:47 +0530

    Trello Clone

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import initialData from "./initial-data";
+import Column from "./Column";
+import { DragDropContext } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-around;
+    background-color: lightblue;
+    height: auto;
+    min-height: 300px;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setdata] = useState(initialData);
+    function onDragEnd(result) {
+        const { destination, source, draggableId } = result;
+        if (!destination) {
+            return;
+        }
+        if (
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        ) {
+            return;
+        }
+
+        const start = data.columns[source.droppableId];
+        const finish = data.columns[destination.droppableId];
+        console.log(start);
+        console.log(finish);
+        if (start === finish) {
+            const newTaskIds = [...start.taskIds];
+            newTaskIds.splice(source.index, 1);
+            newTaskIds.splice(destination.index, 0, draggableId);
+
+            const newColumn = {
+                ...start,
+                taskIds: newTaskIds,
+            };
+
+            const newData = {
+                ...data,
+                columns: {
+                    ...data.columns,
+                    [newColumn.id]: newColumn,
+                },
+            };
+            setdata(newData);
+        } else {
+            const startTaskIds = [...start.taskIds];
+            startTaskIds.splice(source.index, 1);
+            const newStart = {
+                ...start,
+                taskIds: startTaskIds,
+            };
+            const finishTaskIds = [...finish.taskIds];
+            finishTaskIds.splice(destination.index, 0, draggableId);
+            const newFinish = {
+                ...finish,
+                taskIds: finishTaskIds,
+            };
+
+            const newData = {
+                ...data,
+                columns: {
+                    ...data.columns,
+                    [newStart.id]: newStart,
+                    [newFinish.id]: newFinish,
+                },
+            };
+            setdata(newData);
+        }
+    }
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <h1 style={{ textAlign: "center" }}>Trello Clone Using React</h1>
+            <Container>
+                {initialData.columnOrder.map((columnId) => {
+                    const column = data.columns[columnId];
+                    const tasks = column.taskIds.map((tasksId) => {
+                        return data.tasks[tasksId];
+                    });
+                    return (
+                        <Column key={column.id} column={column} tasks={tasks} />
+                    );
+                })}
+            </Container>
+        </DragDropContext>
+    );
 }
 
 export default App;
---
+++ src/Column.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import Task from "./Task";
+import { Droppable } from "react-beautiful-dnd";
+
+const Container = styled.div`
+    margin: 8px;
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+    width: 220px;
+    display: flex;
+    flex-direction: column;
+`;
+const Title = styled.h3`
+    padding: 8px;
+`;
+const TaskList = styled.div`
+    padding: 8px;
+    transition: background-color 1s ease;
+    background-color: ${(props) =>
+        props.isDraggingOver ? "skyblue" : "white"};
+    flex-grow: 1;
+    min-height: 100px;
+`;
+
+function Column(props) {
+    return (
+        <div>
+            <Container>
+                <Title>{props.column.title}</Title>
+                <Droppable droppableId={props.column.id}>
+                    {(provided, snapshot) => (
+                        <TaskList
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                            isDraggingOver={snapshot.isDraggingOver}
+                        >
+                            {props.tasks.map((task, index) => (
+                                <Task key={task.id} task={task} index={index} />
+                            ))}
+                            {provided.placeholder}
+                        </TaskList>
+                    )}
+                </Droppable>
+            </Container>
+        </div>
+    );
+}
+
+export default Column;
